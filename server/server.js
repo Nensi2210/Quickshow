a4e343171adbcde2cc15cf3324a950f51e2abff1
nensi2210 <ghadiyanensi@gmail.com>
2026-03-09 15:37:48 +0530

    fix vercel backend bug

--- server/server.js
+++ server/server.js
@@ -2,33 +2,27 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express';
 import { serve } from "inngest/express";
 import { inngest } from "./inngest/index.js";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => res.send('Server is live!'));
 
 app.use('/api/inngest', serve({
   client: inngest,
-  functions: []   // ✅ FIX
+  functions: []
 }));
 
-const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
-  } catch (error) {
-    console.error('Failed to start server ', error.message);
-  }
-};
+// connect DB
+await connectDB();
 
-startServer();
+// ❗ Vercel ke liye export karna zaroori
+export default app;
